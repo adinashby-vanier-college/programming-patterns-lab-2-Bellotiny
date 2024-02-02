196c63a105cec97f0fad732ea134b5c5862f6a1e
Bellotiny <donissaint13@gmail.com>
2024-02-02 15:42:51 -0500

    Finally Done

--- src/main/java/com/prog2/labs/LabTwo.java
+++ src/main/java/com/prog2/labs/LabTwo.java
@@ -1,5 +1,6 @@
 package com.prog2.labs;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,26 +16,45 @@ public class LabTwo {
 	 *
 	 */
 	public static void main(String[] args) {
+            List<Integer> list = Arrays.asList(1, 3, 5, 2, 8);
+        List<String> list2 = Arrays.asList("22 3 43 12 16");
+        List<Integer> list2a = Arrays.asList(22, 3, 43, 12, 16);
+        List<String> words = Arrays.asList("atom", "saga", "Veil", "aim", "wave", "live", "actual", "ant");
 
-	}
+        System.out.println("Result for Average :" + averageValue(list));
+        System.out.println("Result for Capitalization: " + convertToUpperCase(words));
+        System.out.println("Result for Word Finding: " + filterStrings(words));
+        System.out.println("Result for Labeling Numbers: " + commaSeparatedStrings(list2a));
+    }
 
-	/**
-	 * Please refer to the README file for question(s) description
-	 */
+    /**
+     * Please refer to the README file for question(s) description
+     */
+    public static Double averageValue(List<Integer> list) {
+        return list.stream()
+                .mapToDouble(x -> x)
+                .average()
+                .orElse(Double.NaN);
+    }
 
-	public static Double averageValue(List<Integer> list) {
-		return 0.0;
-	}
+    public static List<String> convertToUpperCase(List<String> list) {
+        return list.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+    }
 
-	public static List<String> convertToUpperCase(List<String> list) {
-		return list;
-	}
+    public static List<String> filterStrings(List<String> list) {
+        return list.stream()
+                .filter(x -> x.startsWith("a") && x.length() == 3)
+                .collect(Collectors.toList());
+    }
 
-	public static List<String> filterStrings(List<String> list) {
-		return list;
-	}
-
-	public static String commaSeparatedStrings(List<Integer> list) {
-		return "";
-	}
+    public static String commaSeparatedStrings(List<Integer> list) {
+        String joinedString = list
+                .stream()
+                .map(x -> x % 2 == 0 ? "e" + Integer.toString(x) : "o" + Integer.toString(x))
+                .collect(Collectors.joining(","));
+        
+        return joinedString;
+    }
 }
